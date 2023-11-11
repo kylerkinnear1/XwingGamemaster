9974cd7f8282ad44831bd4b5061d90135d6ad9ec
Kyle Kinnear <kylerkinnear@gmail.com>
2023-11-11 09:42:51 -0600

    start unit tests

---
+++ XwingTurnRunner.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using AutoFixture.Xunit2;
+using XwingTurnRunner.XWingStuff;
+
+namespace XwingTurnRunner.Tests;
+
+public class UnitTest1
+{
+    [Theory, AutoData]
+    public void Test1(Pilot pilot, Dial dial)
+    {
+    }
+}
