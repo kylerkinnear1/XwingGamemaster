c647f5e58380505039749bc0ee3d5bc5fc8fcdee
Kyle Kinnear <kylerkinnear@gmail.com>
2023-11-11 09:31:07 -0600

    initial commit

---
+++ XwingTurnRunner/XWingStuff/Dial.cs
@@ -0,0 +1,7 @@
+﻿namespace XwingTurnRunner.XWingStuff;
+
+public record Dial(IEnumerable<Maneuver> Maneuvers);
+
+public enum Direction { Left, Right, Straight }
+public enum Bearing { Hard, Bank, KTurn, Sloop, Stop, Reverse }
+public record Maneuver(int Speed, Direction Direction, Bearing Bearing);
---
+++ XwingTurnRunner/XWingStuff/Pilot.cs
@@ -0,0 +1,33 @@
+﻿using static XwingTurnRunner.XWingStuff.Pilot;
+
+namespace XwingTurnRunner.XWingStuff;
+
+public record Player(
+    int Id,
+    List<Ship> Ships,
+    bool HasInitiative);
+
+public record Pilot(
+    int Skill,
+    int Points,
+    Assignment Assignments)
+{
+    public class Assignment
+    {
+        public int Stress { get; set; } = 0;
+        public List<CriticalHit> CriticalHits { get; set; } = new();
+    }
+}
+
+public record CriticalHit(Type Type);
+
+public enum CriticalHitType
+{
+    DirectDamage
+}
+
+public record Ship(Pilot Pilot, ShipType Type, int Hull, int Shields, int ShieldsRemaining, int HealthRemaining);
+public enum ShipType
+{
+    T65, T70, Z95
+}
