32b1dbe00a5b4b4a0e484152dcf1dd8a5321e461
Kyle Kinnear <kylerkinnear@gmail.com>
2023-11-12 14:12:33 -0600

    Add GreatState and Stub out ideas.

---
+++ GreatState/StateMachineWhiteboarding.cs
@@ -0,0 +1,25 @@
+﻿namespace GreatState;
+
+// Somehow, return the next step?
+public interface IStep
+{
+    Task<IStep> Run();
+}
+
+public class Step<TContext> : IStep
+{
+    public Task<IStep> Run()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class EventBus
+{
+
+}
+
+public class StateMachine
+{
+    private readonly IEnumerable<IStep> _steps;
+}
